c41eb548c4f7cfece7c715b173d481edebea8570
Florian Kaiser <37000563+flok@users.noreply.github.com>
2021-11-05 18:06:54 +0100

    Fix

--- playstation_api.py
+++ playstation_api.py
@@ -30,6 +30,7 @@ def get_game_with_url(titleID: str, search_term: str):
 
     url = r'https://web.np.playstation.com/api/graphql/v1//op?operationName=getSearchResults&variables={"countryCode":"US","languageCode":"en","searchTerm":"%s"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"d77d9a513595db8d75fc26019f01066d54c8d0de035a77a559bd687fa1010418"}}' % search_term
     print(f"URL: {url}")
+    print(f"URL repr {url.__repr__()}")
     r = requests.get(url=url)
     print(f"Status Code : {r.status_code}")
     print(f"Json : {r.json()}")
